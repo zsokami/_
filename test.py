fcd2246f7acd18214d825e9c73342988d70d5dd8
zsokami <57834527+zsokami@users.noreply.github.com>
2022-10-23 02:15:13 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import time
+import requests
+
+session = requests.session()
+# session.trust_env = False  # 禁用系统代理
+# session.proxies['http'] = '127.0.0.1:7890'
+# session.proxies['https'] = '127.0.0.1:7890'
+st = time.time()
+with session.get('https://www.google.com/') as res:
+    print(time.time() - st)
+    print(res.status_code)
+st = time.time()
+with session.get('https://www.baidu.com/') as res:
+    print(time.time() - st)
+    print(res.status_code)
