024814014d8c40f4706aacb5a0ba16008749c5cc
钟孟捷 <718335940@qq.com>
2022-11-19 14:21:15 +0800

    test

--- test_undetected_chromedriver.py
+++ test_undetected_chromedriver.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from undetected_chromedriver import Chrome, ChromeOptions
 
-use_proxy = False
+use_proxy = True
 
 
 class Session(requests.Session):
@@ -34,7 +34,8 @@ if __name__ == '__main__':
     def test(url):
         options = ChromeOptions()
         options.add_argument(
-            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        )
         options.page_load_strategy = 'eager'
         chrome = Chrome(
             options=options,
@@ -42,30 +43,35 @@ if __name__ == '__main__':
         )
         wait = WebDriverWait(chrome, 20)
         try:
-            # print(f'get {url}')
-            chrome.get(url)
-            # print('get done, wait...')
-            try:
-                # st = time()
-                wait.until_not(ec.any_of(ec.title_is('Just a moment...'), ec.title_is('')))
-                # print('WebDriverWait', time() - st, 'seconds')
-                # print('title is not "Just a moment..." and not empty')
-            except TimeoutException:
-                # print('WebDriverWait timeout')
-                return None
-            sess = Session(use_proxy=use_proxy, user_agent=chrome.execute_script('return navigator.userAgent'))
-            for key in ['cf_clearance', 'ge_ua_key']:
-                cookie = chrome.get_cookie(key)
-                if cookie:
-                    sess.cookies[key] = cookie['value']
-            # print(sess.headers['User-Agent'])
-            # print(sess.cookies.get_dict())
-            doc = BeautifulSoup(sess.get(url).text, 'html.parser')
-            # print(doc.title)
-            return doc.title
+            for nRetries in range(10):
+                # print(f'get {url}')
+                chrome.get(url)
+                # print('get done, wait...')
+                try:
+                    # st = time()
+                    wait.until_not(ec.any_of(ec.title_is('Just a moment...'), ec.title_is('')))
+                    # print('WebDriverWait', time() - st, 'seconds')
+                    # print('title is not "Just a moment..." and not empty')
+                except TimeoutException:
+                    # print('WebDriverWait timeout')
+                    continue
+                sess = Session(use_proxy=use_proxy, user_agent=chrome.execute_script('return navigator.userAgent'))
+                for key in ['cf_clearance', 'ge_ua_key']:
+                    cookie = chrome.get_cookie(key)
+                    if cookie:
+                        sess.cookies[key] = cookie['value']
+                # print(sess.headers['User-Agent'])
+                # print(sess.cookies.get_dict())
+                doc = BeautifulSoup(sess.get(url).text, 'html.parser')
+                # print(doc.title)
+                if doc.title.text not in ('Just a moment...', ''):
+                    return doc.title, nRetries
+            return None, nRetries
+        except Exception as e:
+            return e, nRetries
         finally:
             chrome.quit()
 
     with ThreadPoolExecutor(32) as executor:
-        for title in executor.map(test, urls):
-            print(title)
+        for res, nRetries in executor.map(test, urls):
+            print(res, nRetries)
