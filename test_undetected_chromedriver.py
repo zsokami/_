78202950a374bd4316a84476bf14190ae4401a35
钟孟捷 <718335940@qq.com>
2022-11-20 15:29:19 +0800

    test

--- test_undetected_chromedriver.py
+++ test_undetected_chromedriver.py
@@ -1,53 +1,44 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
-from time import sleep
-from urllib.parse import urljoin
+# from time import sleep
+# from urllib.parse import urljoin
 
-import requests
+# import requests
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from undetected_chromedriver import Chrome, ChromeOptions
-from seleniumrequests import RequestsSessionMixin
 
-use_proxy = False
-
-
-class Session(requests.Session):
-    def __init__(
-        self,
-        host=None,
-        use_proxy=False,
-        user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-    ):
-        super().__init__()
-        if use_proxy:
-            self.trust_env = False  # 禁用系统代理
-            self.proxies['http'] = self.proxies['https'] = '127.0.0.1:7890'
-        self.headers['User-Agent'] = user_agent
-        self.base = 'https://' + host if host else ''
-        self.host = host
-
-    def request(self, method, url, **kwargs):
-        return super().request(method, urljoin(self.base, url), **kwargs)
-
-    def get_ip_info(self):
-        """return (ip, 位置, 运营商)"""
-        addr = self.get(f'https://ip125.com/api/{self.get("https://ident.me").text}?lang=zh-CN').json()
-        return (
-            addr['query'],
-            addr['country'] + (',' + addr['city'] if addr['city'] and addr['city'] != addr['country'] else ''),
-            addr['isp'] + (',' + addr['org'] if addr['org'] and addr['org'] != addr['isp'] else '')
-        )
+# use_proxy = True
 
 
-class MyChrome(RequestsSessionMixin, Chrome):
-    def __init__(self, *args, proxy_host="127.0.0.1", **kwargs):
-        super().__init__(*args, proxy_host=proxy_host, **kwargs)
-        if use_proxy:
-            self.requests_session.trust_env = False  # 禁用系统代理
-            self.requests_session.proxies['http'] = self.requests_session.proxies['https'] = '127.0.0.1:7890'
+# class Session(requests.Session):
+#     def __init__(
+#         self,
+#         host=None,
+#         use_proxy=False,
+#         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+#     ):
+#         super().__init__()
+#         if use_proxy:
+#             self.trust_env = False  # 禁用系统代理
+#             self.proxies['http'] = self.proxies['https'] = '127.0.0.1:7890'
+#         self.headers['User-Agent'] = user_agent
+#         self.base = 'https://' + host if host else ''
+#         self.host = host
+
+#     def request(self, method, url, **kwargs):
+#         return super().request(method, urljoin(self.base, url), **kwargs)
+
+#     def get_ip_info(self):
+#         """return (ip, 位置, 运营商)"""
+#         addr = self.get(f'https://ip125.com/api/{self.get("https://ident.me").text}?lang=zh-CN').json()
+#         return (
+#             addr['query'],
+#             addr['country'] + (',' + addr['city'] if addr['city'] and addr['city'] != addr['country'] else ''),
+#             addr['isp'] + (',' + addr['org'] if addr['org'] and addr['org'] != addr['isp'] else '')
+#         )
 
 
 if __name__ == '__main__':
@@ -58,28 +49,28 @@ if __name__ == '__main__':
         # options.add_argument(
         #     '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
         # )
-        options.add_argument('--disable-popup-blocking')
+        # options.add_argument('--disable-popup-blocking')
         options.page_load_strategy = 'eager'
 
-        seleniumwire_options = None
-        if use_proxy:
-            seleniumwire_options = {
-                'proxy': {
-                    'http': 'http://127.0.0.1:7890',
-                    'https': 'https://127.0.0.1:7890',
-                }
-            }
+        # seleniumwire_options = None
+        # if use_proxy:
+        #     seleniumwire_options = {
+        #         'proxy': {
+        #             'http': 'http://127.0.0.1:7890',
+        #             'https': 'https://127.0.0.1:7890',
+        #         }
+        #     }
 
-        chrome = MyChrome(
+        chrome = Chrome(
             options=options,
-            seleniumwire_options=seleniumwire_options,
+            # seleniumwire_options=seleniumwire_options,
             driver_executable_path=os.path.join(os.getenv('CHROMEWEBDRIVER'), 'chromedriver')
         )
 
         wait = WebDriverWait(chrome, 20)
         res, nTries, nTimeout = None, 0, 0
         try:
-            for nTries in range(1, 6):
+            for nTries in range(1, 2):
                 # print(f'get {url}')
                 chrome.get(url)
                 if chrome.title not in ('Just a moment...', ''):
@@ -101,13 +92,11 @@ if __name__ == '__main__':
                 #         chrome_headers = req.headers
                 # print('chrome_headers', chrome_headers.as_string())
 
-                title0 = BeautifulSoup(chrome.request('GET', url).text, 'html.parser').title
-
-                sess = Session(use_proxy=use_proxy, user_agent=chrome.execute_script('return navigator.userAgent'))
-                for key in ['cf_clearance', 'ge_ua_key']:
-                    cookie = chrome.get_cookie(key)
-                    if cookie:
-                        sess.cookies[key] = cookie['value']
+                # sess = Session(use_proxy=use_proxy, user_agent=chrome.execute_script('return navigator.userAgent'))
+                # for key in ['cf_clearance', 'ge_ua_key']:
+                #     cookie = chrome.get_cookie(key)
+                #     if cookie:
+                #         sess.cookies[key] = cookie['value']
 
                 # for name in ['cookie', 'cache-control', 'accept', 'accept-language']:
                 #     sess.headers[name] = chrome_headers[name]
@@ -126,16 +115,18 @@ if __name__ == '__main__':
 
                 # print(sess.headers['User-Agent'])
                 # print(sess.cookies.get_dict())
-                sess_res = sess.get(url)
+                # sess_res = sess.get(url)
 
                 # print('session_headers', sess_res.request.headers)
 
-                doc = BeautifulSoup(sess_res.text, 'html.parser')
-
-                # print(doc.title)
-                if doc.title.text not in ('Just a moment...', '') or title0.text not in ('Just a moment...', ''):
-                    res = (doc.title, title0)
-                    break
+                # # print(doc.title)
+                # if doc.title.text not in ('Just a moment...', ''):
+                #     res = doc.title
+                #     break
+                markup = chrome.execute_script(f"return await fetch({repr(url)}).then(r => r.text())")
+                doc = BeautifulSoup(markup, 'html.parser')
+                res = doc.title
+                break
 
         except Exception as e:
             res = e
