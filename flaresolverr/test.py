c7a3887fb6911de670d9b1751b8693a76f7488ac
钟孟捷 <718335940@qq.com>
2023-04-24 01:50:01 +0800

    configure logger

--- flaresolverr/test.py
+++ flaresolverr/test.py
@@ -1,11 +1,34 @@
+import logging
+import os
+import sys
 from func_timeout import FunctionTimedOut, func_timeout
 
 from dtos import V1RequestBase
 from flaresolverr_service import evil_logic
-from utils import get_webdriver
+import utils
 
 if __name__ == '__main__':
-    with get_webdriver() as chrome:
+    # configure logger
+    log_level = os.environ.get('LOG_LEVEL', 'info').upper()
+    logger_format = '%(asctime)s %(levelname)-8s %(message)s'
+    if log_level == 'DEBUG':
+        logger_format = '%(asctime)s %(levelname)-8s ReqId %(thread)s %(message)s'
+    logging.basicConfig(
+        format=logger_format,
+        level=log_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    # disable warning traces from urllib3
+    logging.getLogger('urllib3').setLevel(logging.ERROR)
+    logging.getLogger('selenium.webdriver.remote.remote_connection').setLevel(logging.WARNING)
+    logging.getLogger('undetected_chromedriver').setLevel(logging.WARNING)
+
+    logging.debug('Debug log enabled')
+
+    with utils.get_webdriver() as chrome:
         req = V1RequestBase({'url': 'https://nowsecure.nl/'})
         timeout = 30
         try:
