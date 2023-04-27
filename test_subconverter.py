7ad25485c498226ee1b59f0f293740553d07e1c0
钟孟捷 <718335940@qq.com>
2023-04-27 19:08:13 +0800

    test

--- test_subconverter.py
+++ test_subconverter.py
@@ -1,4 +1,8 @@
 import requests
-print(requests.get('http://localhost/sub?target=clash&list=true&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=https://paste.wmlabs.net/raw/82965bb7b5a3').text)
+from base64 import urlsafe_b64encode
+sub = '''proxies:
+- {name: V1 🇭🇰香港1 IPLC 倍率1x, server: s.hkspeedup.com, port: 4001, type: ss, cipher: aes-256-gcm, password: lr62zu22w7, udp: true}
+'''
+print(requests.get('http://localhost/sub?target=clash&list=true&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=data:text/plain;base64,'+urlsafe_b64encode(sub)).text)
 
 print(requests.get('http://localhost/sub?target=clash&list=true&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=https://api.hkspeedup.com/subscribe/74rk9uwk6m').text)
