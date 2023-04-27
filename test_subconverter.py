2bf7361b10c90411eee40d7bf7a1083f324ac246
钟孟捷 <718335940@qq.com>
2023-04-27 18:41:15 +0800

    test

--- test_subconverter.py
+++ test_subconverter.py
@@ -1,2 +1,4 @@
 import requests
-print(requests.get('http://localhost/sub?target=clash&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=https://paste.wmlabs.net/raw/82965bb7b5a3').text)
+print(requests.get('http://localhost/sub?target=clash&list=true&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=https://paste.wmlabs.net/raw/82965bb7b5a3').text)
+
+print(requests.get('http://localhost/sub?target=clash&list=true&udp=true&scv=true&config=https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ACL4SSR_Online_Full_Mannix.ini&url=https://api.hkspeedup.com/subscribe/74rk9uwk6m').text)
